9e94d271fdc05d70bb6bb6a0d1e5a7c4f27b91af
RicardoGuzmanOrtiz <90017920+RicardoGuzmanOrtiz@users.noreply.github.com>
2024-04-05 20:30:57 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+
+const colors = require('ansi-colors')
+console.log(colors.red('red text'))
+console.log(colors.blue('blue text'))
+console.log(colors.green('green text'))
+
+console.log(colors.bgRed('red backgraund'))
+console.log(colors.black.bgWhite('black text white backgraund'))
